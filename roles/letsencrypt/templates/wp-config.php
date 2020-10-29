a713f2d8c5ba1ed25a273d1599fe8d7477c78f6c
farman17 <ahmadfarman1701@gmail.com>
2020-10-29 14:09:52 +0000

    Edit wp-config

--- roles/letsencrypt/templates/wp-config.php
+++ roles/letsencrypt/templates/wp-config.php
@@ -1,6 +1,4 @@
 <?php
-define( 'WP_SITEURL', 'http://sosialmedia.dailybugis.my.id' );
-define( 'WP_HOME', 'http://sosialmedia.dailybugis.my.id' );
 /**
  * The base configuration for WordPress
  *
@@ -34,7 +32,7 @@ define( 'DB_PASSWORD', '{{ wp_db_password }}' );
 define( 'DB_HOST', '{{ wp_db_hostname }}' );
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
@@ -48,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'j<K%s)Y#:#nb:qmRMPtIZ{yvRH}[`n{bh}eyo!NR@Nn2u`i+p-1_]f,G|}R]` xm' );
-define( 'SECURE_AUTH_KEY',  '(@6dwE>rqC,! @c8nRW,o1xH7&yFq9f8:+|:Mj5>,tim2a$,lp7q[FMr{IQaBtM_' );
-define( 'LOGGED_IN_KEY',    '!MexV/UcZ_7x8Xru}>9,G]&M0tM;,MO/Io!R4shumzRb/uY}cXa{Z]O|fxxY@Z1-' );
-define( 'NONCE_KEY',        'z2fheIER]:MP<*cin.on_=?!owZW6,&#=+YIVcxFm[2)DpO9NJie~b3$|-Hh7B#S' );
-define( 'AUTH_SALT',        'M<WYcrR9xv#DV24m$AprBJn2`x+9Zhg+7A3s#&,)z99NyB&cLDbE[vj+su=z.MXv' );
-define( 'SECURE_AUTH_SALT', 'jsjLn#!MTP<U-0@Y@ZMBdTH4Ne+{l*5!mj:?!^q*<d9^},kXYCBi?Df@=zBW1KD;' );
-define( 'LOGGED_IN_SALT',   '{1kaOG@3p(j:@R_7N/(Ey+YS1@X+&L=^[Z:!C(2+C,s@59I-~m;YUX)dmLwr}7u.' );
-define( 'NONCE_SALT',       'z;W!m^=050<V%e^n#/-l[J9Gg{5klL(aH6l~{tignsBdenuo<Ydp*8m|FnWS8{So' );
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
 
 /**#@-*/
 
@@ -85,7 +83,7 @@ define( 'WP_DEBUG', false );
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+        define( 'ABSPATH', __DIR__ . '/' );
 }
 
 /** Sets up WordPress vars and included files. */
